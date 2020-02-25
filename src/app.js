70c49a45a20fdeaf71e085658f52cb4fb8c6a318
souru98 <kulkarnisourabh1998@gmail.com>
2020-02-25 15:26:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,10 +23,16 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers = PLAYERS.map(function(name, strength, image, type) {
+        name: PLAYERS[i];
+        strength: getRandomStrength();
+        image: "./images/super-" + (i + 1) + ".png";
+        type: "hero|villan"
+    });
 
     // Instead of forloop use Map method
     // Code here
+
 
     return detailedPlayers;
 }
@@ -38,11 +44,20 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
+    var fragment = "";
+    for (var i = 0; i < 4; i++) {
+        fragment = `
+            <div class="player">
+            <img src="${detailedPlayers[i].image}" alt = "characters">
+            <h4> ${detailedPlayers[i].name}</h4>
+            <h1>${detailedPlayers[i].strength}</h1>
+            </div>
+             `;
+    }
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+
 
     return fragment;
 }
